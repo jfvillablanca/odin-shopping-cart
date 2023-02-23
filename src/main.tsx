d75c14ff7a8e7bdd583432b10cf71bd0281ccc0e
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-02-23 20:23:55 +0800

    Refactor: use createRouterConfig in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,26 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
-import App from "./routes/App";
-import Home from "./routes/Home";
-import Shop from "./routes/Shop";
+import createRouterConfig from "./routerConfig";
 
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <App />,
-        children: [
-            {
-                path: "home/",
-                element: <Home />,
-            },
-            {
-                path: "shop/",
-                element: <Shop />,
-            },
-        ],
-    },
-]);
+const router = createBrowserRouter(createRouterConfig());
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
