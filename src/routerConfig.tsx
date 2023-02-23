334984ebf09d0dbe5a27e238e6697f06cbaa40d1
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-02-23 20:21:42 +0800

    Feat: createRouterConfig()

---
+++ src/routerConfig.tsx
@@ -0,0 +1,26 @@
+import App from "./routes/App";
+import Home from "./routes/Home";
+import Shop from "./routes/Shop";
+
+export default function createRouterConfig() {
+    return [
+        {
+            path: "/",
+            element: <App />,
+            children: [
+                {
+                    path: "",
+                    element: <Home />,
+                },
+                {
+                    path: "home/",
+                    element: <Home />,
+                },
+                {
+                    path: "shop/",
+                    element: <Shop />,
+                },
+            ],
+        },
+    ];
+}
