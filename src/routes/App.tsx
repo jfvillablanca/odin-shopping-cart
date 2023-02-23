0fb500fb0c3e617daaf181479a5877da9a2b32b1
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-02-23 10:56:45 +0800

    Style: set App to fill screen

--- src/routes/App.tsx
+++ src/routes/App.tsx
@@ -3,7 +3,7 @@ import Nav from "./Nav";
 
 function App() {
     return (
-        <div className='App'>
+        <div className='App h-screen w-screen'>
             <Nav />
         </div>
     );
