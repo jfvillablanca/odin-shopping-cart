8109723af1061b960d5129aa19412bc60591573a
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-02-23 09:35:47 +0800

    Test: smoke test for Home and Shop

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,23 @@
 import { render } from "@testing-library/react";
 import { describe, test } from "vitest";
+import Home from "./routes/Home";
 import Nav from "./routes/Nav";
+import Shop from "./routes/Shop";
 
 describe("Nav tests", () => {
     test("renders without crashing", () => {
         render(<Nav />);
+    });
+});
+
+describe("Home tests", () => {
+    test("renders without crashing", () => {
+        render(<Home />);
+    });
+});
+
+describe("Shop tests", () => {
+    test("renders without crashing", () => {
+        render(<Shop />);
     });
 });
