6c32128e908364dd9e8ae6aa4435ada9cb34bd50
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-02-23 10:55:08 +0800

    Style: basic layouting for Nav

--- src/routes/Nav.tsx
+++ src/routes/Nav.tsx
@@ -1,12 +1,14 @@
 function Nav() {
     return (
-        <nav>
-            <ul>
+        <nav className='flex items-center justify-between bg-gray-600 w-screen px-9 h-14'>
+            <h1 className='flex-1 text-lg'>Logo here</h1>
+            <ul className='flex flex-1 items-center justify-around list-none'>
                 <li>Home</li>
                 <li>Shop</li>
             </ul>
+            <button>Add to Cart</button>
         </nav>
     );
 }
 
-export default Nav
+export default Nav;
