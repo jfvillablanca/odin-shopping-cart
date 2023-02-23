27335fcedd79606086e42db87dbb4948a2fab797
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-02-23 09:38:36 +0800

    Chore(org): move App.test to tests/

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,8 +1,8 @@
 import { render } from "@testing-library/react";
 import { describe, test } from "vitest";
-import Home from "./routes/Home";
-import Nav from "./routes/Nav";
-import Shop from "./routes/Shop";
+import Home from "../routes/Home";
+import Nav from "../routes/Nav";
+import Shop from "../routes/Shop";
 
 describe("Nav tests", () => {
     test("renders without crashing", () => {
