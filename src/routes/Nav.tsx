0f94aee8c610afc6a223545d0a420520c863c1b6
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-02-24 20:24:38 +0800

    Feat: add home and shop links

--- src/routes/Nav.tsx
+++ src/routes/Nav.tsx
@@ -1,10 +1,16 @@
+import { Link } from "react-router-dom";
+
 function Nav() {
     return (
         <nav className='flex items-center justify-between bg-gray-600 w-screen px-9 h-14'>
             <h1 className='flex-1 text-lg'>Logo here</h1>
             <ul className='flex flex-1 items-center justify-around list-none'>
-                <li>Home</li>
-                <li>Shop</li>
+                <li>
+                    <Link to='/home'>Home</Link>
+                </li>
+                <li>
+                    <Link to='/shop'>Shop</Link>
+                </li>
             </ul>
             <button>Add to Cart</button>
         </nav>
