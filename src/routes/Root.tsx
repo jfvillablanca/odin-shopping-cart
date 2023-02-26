25e681e9adf8fd8676ffbd30f6877eebd1889d01
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-02-26 11:16:52 +0800

    Feat: add Nav state for icon click

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,10 +1,16 @@
+import { useState } from "react";
 import { Outlet } from "react-router-dom";
 import Nav from "./Nav";
 
 function Root() {
+    const [isBagOpen, setIsBagOpen] = useState(false);
+
+    const handleBagIconClick = () =>
+        setIsBagOpen((prevIsBagOpen) => !prevIsBagOpen);
+
     return (
         <div className='h-screen w-screen'>
-            <Nav />
+            <Nav isBagOpen={isBagOpen} handleClick={handleBagIconClick} />
             <Outlet />
         </div>
     );
