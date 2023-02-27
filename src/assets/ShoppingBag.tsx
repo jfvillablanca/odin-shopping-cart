5a535ec326801bbde2eae1e60c5a2d817f780b56
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-02-27 10:28:03 +0800

    Chore: format and cleanup of asset

--- src/assets/ShoppingBag.tsx
+++ src/assets/ShoppingBag.tsx
@@ -1,15 +1,25 @@
-import React from 'react';
-
-interface ShoppingBagProps {
-  className?: string;
+interface Props {
+    className?: string;
 }
 
-export default function ShoppingBag(props: ShoppingBagProps) {
-  return (
-    <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="feather feather-shopping-bag" {...props}>
-      <path d="M6 2L3 6v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V6l-3-4z" />
-      <line x1={3} y1={6} x2={21} y2={6} />
-      <path d="M16 10a4 4 0 0 1-8 0" />
-    </svg>
-  );
+export default function ShoppingBag(props: Props) {
+    return (
+        <svg
+            xmlns='http://www.w3.org/2000/svg'
+            width={24}
+            height={24}
+            viewBox='0 0 24 24'
+            fill='none'
+            stroke='currentColor'
+            strokeWidth={2}
+            strokeLinecap='round'
+            strokeLinejoin='round'
+            className='feather feather-shopping-bag'
+            {...props}
+        >
+            <path d='M6 2L3 6v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V6l-3-4z' />
+            <line x1={3} y1={6} x2={21} y2={6} />
+            <path d='M16 10a4 4 0 0 1-8 0' />
+        </svg>
+    );
 }
